2c248aff3925dfa80a735a8835e8df2bf77e0f24
Pinto Manuel <pinto@fireworkx.co.za>
2023-01-11 14:29:41 +0200

    Login and register url is added

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+app.post('/api/register', (req, res) => {
+    console.log(req.body);
+    res.json({ status: 'ok'})
+})
+
+app.listen(8080, () => {
+    console.log('server started on port 8080');
+} )
